897be458af84c771c04fd632beff8a58cc88f181
ky-s <kyohei_86@hotmail.com>
2020-03-26 00:50:58 +0900

    [clean] delete unuse controller code

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,6 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy, :done]
 
   # GET /tasks
-  # GET /tasks.json
   def index
     @tasks = Task.all
     @new_task = Task.new
@@ -14,51 +13,23 @@ class TasksController < ApplicationController
   end
 
   # POST /tasks
-  # POST /tasks.json
   def create
     @task = Task.new(task_params)
 
-    respond_to do |format|
-      if @task.save
-        format.html { redirect_to @task, notice: 'Task was successfully created.' }
-        format.js
-        format.json { render :show, status: :created, location: @task }
-      else
-        format.html { render :new }
-        format.js
-        format.json { render json: @task.errors, status: :unprocessable_entity }
-      end
-    end
+    @task.save
   end
 
   # PATCH/PUT /tasks/1
-  # PATCH/PUT /tasks/1.json
   def update
-    respond_to do |format|
-      if @task.update(task_params)
-        format.html { redirect_to @task, notice: 'Task was successfully updated.' }
-        format.js
-        format.json { render :show, status: :ok, location: @task }
-      else
-        format.html { render :edit }
-        format.js
-        format.json { render json: @task.errors, status: :unprocessable_entity }
-      end
-    end
+    @task.update(task_params)
   end
 
   # DELETE /tasks/1
-  # DELETE /tasks/1.json
   def destroy
     @task.destroy
-    respond_to do |format|
-      format.html { redirect_to tasks_url, notice: 'Task was successfully destroyed.' }
-      format.json { head :no_content }
-    end
   end
 
   def done
-    p @task
     @task.done!
   end
 
