c26463508c37895f32dd3616642d43526c8ac765
ky-s <kyohei_86@hotmail.com>
2020-03-26 14:53:19 +0900

    [update] add task-hidden class on view

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,6 +1,6 @@
 module TasksHelper
   def task_tr(task)
-    classes = task.done? ? 'table-success task-done' : ''
+    classes = task.done? ? 'table-success task-done task-hidden' : ''
     classes = classes + ' task-deadline' if task.deadline?
 
     content_tag :tr, id: "task-#{task.id}", class: classes do
